2a0c08277273b6270b45d0e79aa97ce091a286af
jacketsj <jacketsj@gmail.com>
2019-11-05 05:14:43 +0000

    Mostly finish precomputed bitmask

--- precomputed_bitmask.cpp
+++ precomputed_bitmask.cpp
@@ -11,10 +11,10 @@ typedef long long ll;
 #define WIDTH 10
 #define HEIGHT 10
 //boats
-const vector<int> sizes = {2,3,3,4,5};
+const vector<int> lengths = {2,3,3,4,5};
 */
-const vector<int> sizes = {2,3,3};
-const int n = sizes.size();
+const vector<int> lengths = {2,3,3};
+const int n = lengths.size();
 
 typedef vector<ll> grid_t;
 
@@ -94,7 +94,7 @@ bool draw_state(int state_index, int length, ll mult, grid_t &grid)
 }
 
 // returns a list of state indeces that do not conflict with misses
-vector<int> find_valid_individual(int length, grid_t &misses)
+vector<int> find_valid_individual_states(int length, grid_t &misses)
 {
 	vector<int> res; // final result
 	// check validity of each state index
@@ -107,6 +107,94 @@ vector<int> find_valid_individual(int length, grid_t &misses)
 		draw_state(state_index, length, -1, misses);
 	}
 	return res;
+}
+
+// returns a list of position indeces that do not conflict with misses for each ship
+vector<vector<int>> find_valid_states(grid_t &misses)
+{
+	vector<vector<int>> res(n);
+	for (int i = 0; i < n; ++i)
+		res[i] = find_valid_individual_states(lengths[i], misses);
+	return res;
+}
+
+typedef bitset<WIDTH*(HEIGHT-1)+HEIGHT*(WIDTH-1)> pos_set;
+
+bool compatible_pair(int length_a, int state_a, int length_b, int state_b, grid_t &test_grid)
+{
+	bool valid = draw_state(state_a,length_a,1,test_grid);
+	valid = draw_state(state_b,length_b,1,test_grid) && valid;
+	draw_state(state_a,length_a,-1,test_grid);
+	draw_state(state_b,length_b,-1,test_grid);
+	return valid;
+}
+
+pos_set all_compatible_pairs(int length_a, int state_a, int length_b, vector<int> &states_b, grid_t &test_grid)
+{
+	pos_set res;
+	int i = 0;
+	for (int i = 0; i < states_b.size(); ++i)
+	{
+		int state_b = states_b[i];
+		if (compatible_pair(length_a, state_a, length_b, state_b, test_grid))
+			res[i] = true;
+	}
+	return res;
+}
+
+vector<vector<pos_set>> find_all_pos_sets(vector<vector<int>> &valid_states)
+{
+	// create a single test grid to avoid reallocating memory needlessly
+	grid_t test_grid = create_grid();
+	// indexed by ship1, ship2, position of ship1, ship2
+	vector<vector<vector<pos_set>>> res(n,vector<vector<pos_set>>(n));
+	for (int i = 0; i < lengths.size(); ++i) // for each ship
+		for (int state_i = 0; state_i < valid_states[i].size(): ++state_i) // for each position of ship i
+			for (int j = i+1; j < lengths.size(); ++j) // for each ship that will be seen later than i
+				res[i][j] = all_compatible_pairs(lengths[i], valid_states[i][state_i], lengths[j], valid_states[j], test_grid);
+	return res;
+}
+
+ll place_ship(const int ship_index, const vector<vector<pos_set>> &validity_masks,
+		vector<pos_set> &currently_valid, vector<vector<int>> &state_frequency, const vector<int> &num_valid_states)
+{
+	// base case: all ships placed successfully
+	if (ship_index == n)
+		return 1;
+
+	// iterate over all the ship states that are still valid
+	// TODO: remove the wasteful loops (currently, this code will run about 10^11 times)
+	//       use __builtin_ffsll (returns 1 + the index of the first set bit, or 0 if no set bits)
+	//       this is a (forward) bitscan. MSVC also has a function for it (_BitScanForward64)
+	for (int state_index = 0; state_index < num_valid_states[ship_index]; ++state_index)
+	{
+		if (currently_valid[ship_index][state_index])
+		{
+
+		}
+	}
+}
+
+// list of hits not currently supported, need to derive new algorithm
+// count the number of occurrences of each spot on the grid
+// do this by counting the frequency of each position for each ship
+grid_t count_occurrences(grid_t &misses)
+{
+	vector<vector<int>> valid_states = find_valid_states(misses);
+	// count the number of valid states, to be used when placing ships
+	vector<int> num_valid_states(n);
+	for (int i = 0; i < n; ++i)
+		num_valid_states[i] = valid_states[i].size();
+
+	vector<vector<pos_set>> validity_masks = find_all_pos_sets(valid_states);
+
+	vector<pos_set> currently_valid; // for each ship, keep track of which positions are still valid
+
+	vector<vector<int>> state_frequency(n); // for each ship, keep the frequency of each of its states
+	for (int i = 0; i < n; ++i)
+		state_frequency.resize(valid_states[i].size());
+
+	ll total_states = place_ship(0, validity_masks, currently_valid, state_frequency, num_valid_states);
 }
 
 int main()
