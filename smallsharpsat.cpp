da8c5b7ab5a83f00cf460ab31acd27a92cf74d71
j <jacketsj@gmail.com>
2020-01-30 23:33:01 -0800

    Add smaller sharpsat formulation, minus bitblasting

---
+++ smallsharpsat.cpp
@@ -0,0 +1,248 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+// CONSTANTS
+// board dimensions
+#define WIDTH 10
+#define HEIGHT 10
+/*
+#define WIDTH 10
+#define HEIGHT 10
+//boats
+const vector<int> lengths = {2,3,3,4,5};
+*/
+const vector<int> lengths = {5,4,3,3,2};
+const int n = lengths.size();
+
+//typedef bitset<WIDTH*(HEIGHT-1)+HEIGHT*(WIDTH-1)> pos_set;
+
+typedef vector<ll> grid_t;
+
+grid_t create_grid()
+{
+	return grid_t(WIDTH*HEIGHT);
+}
+
+// produce the number of unique horizontal or vertical positions that a
+// top-left coordinate can take on in the corresponding orientation
+int horizontal_state_class_count(int length)
+{
+	return (WIDTH - length + 1);
+}
+int vertical_state_class_count(int length)
+{
+	return (HEIGHT - length + 1);
+}
+// produce number of horizontal states for a single ship on a clean board
+int horizontal_state_count(int length)
+{
+	return horizontal_state_class_count(length)*HEIGHT;
+}
+int vertical_state_count(int length)
+{
+	return vertical_state_class_count(length)*WIDTH;
+}
+// produce the number of states for a ship on a clean board
+int state_count(int length)
+{
+	return horizontal_state_count(length) + vertical_state_count(length);
+}
+
+// (x,y,dx,dy)
+typedef tuple<int,int,int,int> dpos_t;
+
+// given a state index and a ship length, produce (x,y,dx,dy)
+dpos_t get_state_dpos(int state_index, int length)
+{
+	// define the canonical ordering to be left to right,
+	// top to bottom, horizontal to vertical
+	// all are defined by their top-left coordinate
+
+	// number of states in a given row for the horizontal orientation
+	int horizontal_width = horizontal_state_class_count(length);
+
+	// number of states with horizontal orientation
+	int num_in_horizontal = horizontal_width*HEIGHT;
+	if (state_index < num_in_horizontal)
+	{
+		// horizontal orientation
+		return make_tuple(state_index % horizontal_width, state_index / horizontal_width, 1,0);
+	}
+	//vertical orientation
+	state_index %= num_in_horizontal;
+	return make_tuple(state_index % WIDTH, state_index / WIDTH, 0,1);
+}
+
+// given a state index and a ship length, draw it onto a grid with a multiplier,
+// and return false if a conflict is found, but only after finishing the drawing
+bool draw_state(int state_index, int length, ll mult, grid_t &grid)
+{
+	int x, y, dx, dy;
+	tie(x,y,dx,dy) = get_state_dpos(state_index, length);
+	bool ret = true;
+	for (int l = 0; l < length; ++l)
+	{
+		grid[x+y*WIDTH] += mult;
+
+		if (grid[x+y*WIDTH] > 1) // this grid cell is covered by two ships, so the drawing is invalid
+			ret = false;
+
+		x += dx;
+		y += dy;
+	}
+	return ret;
+}
+
+// returns a list of state indeces that do not conflict with misses
+vector<int> find_valid_individual_states(int length, grid_t &misses)
+{
+	vector<int> res; // final result
+	// check validity of each state index
+	for (int state_index = 0; state_index < state_count(length); ++state_index)
+	{
+		// draw and find validity
+		if (draw_state(state_index, length, 1, misses))
+			res.push_back(state_index);
+		// undraw
+		draw_state(state_index, length, -1, misses);
+	}
+	return res;
+}
+
+// returns a list of position indeces that do not conflict with misses for each ship
+vector<vector<int>> find_valid_states(grid_t &misses)
+{
+	vector<vector<int>> res(n);
+	for (int i = 0; i < n; ++i)
+		res[i] = find_valid_individual_states(lengths[i], misses);
+	return res;
+}
+
+bool compatible_pair(int length_a, int state_a, int length_b, int state_b, grid_t &test_grid)
+{
+	bool valid = draw_state(state_a,length_a,1,test_grid);
+	valid = draw_state(state_b,length_b,1,test_grid) && valid;
+	draw_state(state_a,length_a,-1,test_grid);
+	draw_state(state_b,length_b,-1,test_grid);
+	return valid;
+}
+
+vector<bool> all_compatible_pairs(int length_a, int state_a, int length_b, vector<int> &states_b, grid_t &test_grid)
+{
+	vector<bool> res(states_b.size());
+	for (size_t i = 0; i < states_b.size(); ++i)
+	{
+		int state_b = states_b[i];
+		if (compatible_pair(length_a, state_a, length_b, state_b, test_grid))
+			res[i] = true;
+	}
+	return res;
+}
+
+vector<vector<vector<vector<bool>>>> find_all_nonadj_lists(vector<vector<int>> &valid_states)
+{
+	// create a single test grid to avoid reallocating memory needlessly
+	grid_t test_grid = create_grid();
+	// indexed by ship1, position of ship1, ship2
+	vector<vector<vector<vector<bool>>>> res(n);
+	for (int i = 0; i < n; ++i)
+	{
+		res[i].resize(valid_states[i].size());
+		for (size_t state_i = 0; state_i < valid_states[i].size(); ++state_i) // for each state of ship i
+			res[i][state_i].resize(n);
+	}
+
+	for (int i = 0; i < n; ++i) // for each ship
+		for (size_t state_i = 0; state_i < valid_states[i].size(); ++state_i) // for each state of ship i
+			for (int j = i+1; j < n; ++j) // for each ship that will be seen later than i
+				res[i][state_i][j] = all_compatible_pairs(lengths[i], valid_states[i][state_i], lengths[j], valid_states[j], test_grid);
+	return res;
+}
+
+// print the final grid
+void print_grid(const grid_t &a)
+{
+	for (int y = 0; y < HEIGHT; ++y)
+		for (int x = 0; x < WIDTH; ++x)
+			cout << a[x+y*WIDTH] << "\t\n"[x == WIDTH-1];
+}
+
+// print the final grid of probabilities
+void print_grid_chance(const grid_t &a, const ll &added_count)
+{
+	cout << fixed << setprecision(3); //show 3 decimals for chance
+	double count = added_count;
+	for (int y = 0; y < HEIGHT; ++y)
+		for (int x = 0; x < WIDTH; ++x)
+			cout << double(a[x+y*WIDTH])/count << "\t\n"[x == WIDTH-1];
+}
+
+void gen_cnf(vector<int> num_valid_states, vector<vector<vector<vector<bool>>>> &validity_masks)
+{
+	vector<string> bitvec(n); // index of bitvector
+	for (int i = 0; i < n; ++i)
+		bitvec[i] = i;
+
+	for (int i = 0; i < n; ++i)
+	{
+		cout << "0 <= " << i << '\n';
+		cout << i << " < " << num_valid_states[i] << '\n';
+	}
+	for (int i = 0; i < n; ++i)
+		for (int state_i = 0; state_i < num_valid_states[i]; ++state_i)
+			for (int j = i+1; j < n; ++j)
+				for (int state_j = 0; state_j < num_valid_states[j]; ++state_j)
+					if (!validity_masks[i][state_i][j][state_j])
+						cout << "(" << i << " != " << state_i << ") or (" << j << " != " << state_j << ")\n";
+}
+
+// list of hits not currently supported, need to derive new algorithm
+// count the number of occurrences of each spot on the grid
+// do this by counting the frequency of each position for each ship
+void count_occurrences(grid_t &misses)
+{
+	vector<vector<int>> valid_states = find_valid_states(misses);
+	// count the number of valid states, to be used when placing ships
+	vector<int> num_valid_states(n);
+	for (int i = 0; i < n; ++i)
+		num_valid_states[i] = valid_states[i].size();
+
+	vector<vector<vector<vector<bool>>>> validity_masks = find_all_nonadj_lists(valid_states);
+
+	gen_cnf(num_valid_states,validity_masks);
+
+	//vector<pos_set> currently_valid(n); // for each ship, keep track of which states are still valid
+	//for (int i = 0; i < n; ++i)
+	//	for (int state_i = 0; state_i < num_valid_states[i]; ++state_i)
+	//		currently_valid[i][state_i] = true;
+
+	//vector<vector<int>> state_frequency(n); // for each ship, keep the frequency of each of its states
+	//for (int i = 0; i < n; ++i)
+	//	state_frequency[i].resize(num_valid_states[i]);
+
+	//ll total_successful = 0;
+	//// call recursive ship placement routine to iterate through all valid placements
+	//ll total_states = place_ship(0, validity_masks, currently_valid, state_frequency, num_valid_states, total_successful);
+
+	//grid_t frequencies = create_grid();
+	//for (int i = 0; i < n; ++i)
+	//	for (int state_index = 0; state_index < num_valid_states[i]; ++state_index)
+	//		draw_state(state_index, lengths[i], state_frequency[i][state_index], frequencies);
+
+	//cout << "Total states: " << total_states << endl;
+	//cout << "Total successful (this should be the same number): " << total_successful << endl;
+	//print_grid(frequencies);
+	//print_grid_chance(frequencies,total_states);
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(NULL);
+
+	// no misses for now, empty grid
+	grid_t misses = create_grid();
+	count_occurrences(misses);
+}
